3d74c012c2917b7d285a091e0fc507ebdb20d631
Bassim Ghaly <bassim.ghaly@gmail.com>
2025-11-26 23:54:37 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// OpenModal by click "Buy Now"
+    function openModal(productName) {
+      document.getElementById('modal').style.display = "block";
+      document.getElementById('product-name').innerText = "Product: " + productName;
+    }
+
+    // CloseModal
+    function closeModal() {
+      document.getElementById('modal').style.display = "none";
+    }
+
+    // Confirm Buying (fake)
+    function submitForm() {
+      alert("✅ Ordered Successfully!");
+      closeModal();
+    }
+
+    // CloseModal by click outside Form
+    window.onclick = function(event) {
+      const modal = document.getElementById('modal');
+      if (event.target == modal) {
+        closeModal();
+      }
+    }
